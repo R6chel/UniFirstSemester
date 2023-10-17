3549b3b319b076eb56adc600def1027ecd94b11e
R6chel <92124414+R6chel@users.noreply.github.com>
2023-10-18 00:40:34 +0200

    changed lab5/a1

--- Lab5/assign1.cpp
+++ Lab5/assign1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() 
 {
-	double tab[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 13};
+	double tab[10] = {1, 2, 3.14, 4, 5, 6, 7, 8, 9, 13};
     double sum = 0;
 
 	for (int i = 0; i < 10; i++) {
-        sum = sum + tab[i];
+        sum += tab[i];
     } 
 	cout << sum << endl;
 
