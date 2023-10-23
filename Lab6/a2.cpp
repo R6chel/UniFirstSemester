a3eade1d19b013d11b580b70182584fe931e6200
R6chel <92124414+R6chel@users.noreply.github.com>
2023-10-23 11:09:52 +0200

    Created WIP a2.cpp

---
+++ Lab6/a2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string myString;
+	int number;
+
+	cout << "Input a string: ";
+	getline(cin, myString);
+	cout << myString << "\n";
+	
+
+
+	return 0;
+}
