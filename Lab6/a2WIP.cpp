848db6303e6a19cec47f2f1b941d14d46966906f
R6chel <92124414+R6chel@users.noreply.github.com>
2023-10-24 00:25:56 +0200

    changed name on a2

---
+++ Lab6/a2WIP.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+<<<<<<< HEAD
+#include <string> 
+=======
+#include <string>
+>>>>>>> a3eade1d19b013d11b580b70182584fe931e6200
+
+using namespace std;
+
+int main() {
+<<<<<<< HEAD
+
+    string word;
+    
+    cout << "Input a string: ";
+    getline(cin, word);
+    cout << word << "\n";
+
+    return 0;
+}
+=======
+	string myString;
+	int number;
+
+	cout << "Input a string: ";
+	getline(cin, myString);
+	cout << myString << "\n";
+	
+
+
+	return 0;
+}
+>>>>>>> a3eade1d19b013d11b580b70182584fe931e6200
