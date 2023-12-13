e1841324ea400bb8192ee5e76df88f10f8e519d3
R6chel <92124414+R6chel@users.noreply.github.com>
2023-12-13 23:03:50 +0100

    added lab17\a1.cpp

--- Lab17/a1.cpp
+++ Lab17/a1.cpp
@@ -2,9 +2,28 @@
 #include <stack>
 using namespace std;
 
+void reverseStack(stack<int> &original, stack<int> &reversed) {
 
+    while (!original.empty()) {
+        reversed.push(original.top());
+        original.pop();
+    }
+}
 
 int main() {
 
+    stack<int> myStack;
+    myStack.push(63);
+    myStack.push(5746);
+    myStack.push(534);
+
+    stack<int> reversedStack;
+    reverseStack(myStack, reversedStack);
+
+    while (!reversedStack.empty()) {
+        cout << reversedStack.top() << "\n";
+        reversedStack.pop();
+    }
+    
     return 0;
 }
